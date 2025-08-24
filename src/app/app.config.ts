8a4577129edd4a89cfc2ab0bfd2563c66bab45b3
renan <renan.ferreira.p@gmail.com>
2025-08-24 18:51:57 -0300

    Troca de cores do darkmode

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,21 +6,49 @@ import { httpInterceptor } from './services/http.interceptor';
 //PrimeNG Providers
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeng/themes';
 import Aura from '@primeng/themes/aura';
 
 
-const defaultColors = {  
-    200: '#000000', 
-    500: '#272727ff',  
-};
-const myColorPage = {
-    ...Aura,
-    semantic: {
-        ...Aura.semantic,
-        primary: defaultColors,
-    }
-};
 
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        colorScheme: {
+            light: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{zinc.50}',
+                    100: '{zinc.100}',
+                    200: '{zinc.200}',
+                    300: '{zinc.300}',
+                    400: '{zinc.400}',
+                    500: '{zinc.500}',
+                    600: '{zinc.600}',
+                    700: '{zinc.700}',
+                    800: '{zinc.800}',
+                    900: '{zinc.900}',
+                    950: '{zinc.950}'
+                }
+            },
+            dark: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{slate.50}',
+                    100: '{slate.100}',
+                    200: '{slate.200}',
+                    300: '{slate.300}',
+                    400: '{slate.400}',
+                    500: '{slate.500}',
+                    600: '{slate.600}',
+                    700: '{slate.700}',
+                    800: '{slate.800}',
+                    900: '{slate.800}',
+                    950: '{slate.950}'
+                }
+            }
+        }
+    }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -29,7 +57,7 @@ export const appConfig: ApplicationConfig = {
 
     providePrimeNG({
       theme: {
-        preset: myColorPage,
+        preset: MyPreset,
 
       },
       ripple: true
